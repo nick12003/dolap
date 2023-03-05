2ab1f163687e4463e465f750b279f9e38c3ba686
nick12003 <34929382+nick12003@users.noreply.github.com>
2023-03-05 21:13:01 +0800

    fix: product api檔案修正

--- src/pages/api/products/[pId].js
+++
@@ -1,8 +0,0 @@
-import products from '@/dataBase';
-
-export default function handler(req, res) {
-  const { pid } = req.query;
-  res
-    .status(200)
-    .json({ result: products.filter((product) => product.pid.toString() === pid)?.[0] });
-}
